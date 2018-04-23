14c3cdd889eed17b7f9ed8e41c8d837b13c92ebb
Jason Merecki <jasonmerecki@gmail.com>
2018-04-22 21:41:28 -0600

    starting the js and web3 integration

--- truffle-config.js
+++ truffle-config.js
@@ -8,6 +8,7 @@ module.exports = {
       network_id: "*" // Match any network id
     },
     localGeth: {
+      from: "0x0000000000000000000000000000000000000001",
       host: "127.0.0.1",
       port: 8545,
       network_id: "*"
